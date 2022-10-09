a72ea6bbaa2124f4e81a898f277522cf0de82c33
Estelle Ariane <estellesandjong@gmail.com>
2022-10-09 15:18:36 -0400

    changes

--- Software2KnowledgeCheck1/Software2KnowledgeCheck1/Apartment.cs
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/Apartment.cs
@@ -12,5 +12,15 @@ namespace Software2KnowledgeCheck1
         public int NumberOfOpenUnits { get; set; }
 
         public bool HasParking { get; set; }
+
+
+        //public Apartment(string Street, int StreetNumber, int NumberOfUnits, int NumberOfOpenUnits, bool HasParking)
+        //{
+        //    this.Street = Street;
+        //    this.StreetNumber = StreetNumber;                
+        //    this.NumberOfUnits = NumberOfUnits;
+        //    this.NumberOfOpenUnits = NumberOfOpenUnits;
+        //    this.HasParking = HasParking;                    
+        //}
     }
 }
---
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/CityManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    internal class CityManager
+    {
+        public City CurrentCity { get; set; }
+
+        public void CreateApartment(Apartment apartment)
+        {
+            Apartment ap = new Apartment();
+            // Get materials
+            var materialRepo = new MaterialsRepo();
+            var materialsNeeded = materialRepo.GetMaterials();
+
+            var permitRepo = new ZoningAndPermitRepo();
+
+            var buildingWasMade = ConstructBuilding<Apartment>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
+
+            if (buildingWasMade)
+            {
+                CurrentCity.Buildings.Add(apartment);
+            }
+        }
+
+        public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
+        {
+            if (permit && zoning)
+            {
+                foreach (var material in materials)
+                {
+                    if (material == "concrete")
+                    {
+                        // start laying foundation
+                    }
+                    else if (material == "Steel")
+                    {
+                        // Start building structure
+                    }
+                    // etc etc...
+
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
